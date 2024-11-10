c3764b6ccdabf7cea840d8e7f40701ca2a801f43
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-11-10 22:13:03 +0100

    add sound effect

--- src/script.js
+++ src/script.js
@@ -35,6 +35,20 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
+ * Sounds
+ */
+const hitSound = new Audio('/sounds/hit.mp3');
+
+const playHitSound = (collision) => {
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+    if (impactStrength > 1.5) {
+        hitSound.volume = Math.random();
+        hitSound.currentTime = 0;
+        hitSound.play();
+    }
+}
+
+/**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
@@ -211,6 +225,9 @@ const createSphere = (radius, position) => {
         material: defaultMaterial
     });
     body.position.copy(position);
+    body.addEventListener('collide', (collision) => {
+        playHitSound(collision);
+    });
     world.addBody(body);
 
     // Save in objects to update
@@ -243,6 +260,9 @@ const createBox = (width, height, depth, position) => {
         material: defaultMaterial
     });
     body.position.copy(position);
+    body.addEventListener('collide', (collision) => {
+        playHitSound(collision);
+    });
     world.addBody(body);
 
     // Save in objects to update
