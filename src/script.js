820e831cb5931026c92810d54bb7448050c77be5
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-11-10 20:09:16 +0100

    add create box util function

--- src/script.js
+++ src/script.js
@@ -7,6 +7,23 @@ import CANNON from 'cannon'
  * Debug
  */
 const gui = new GUI();
+const debugObject = {};
+debugObject.createSphere = () => {
+    createSphere(Math.random() * 0.5, {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3
+    });
+};
+debugObject.createBox = () => {
+    createBox(Math.random(), Math.random(), Math.random(), {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3
+    });
+};
+gui.add(debugObject, 'createSphere');
+gui.add(debugObject, 'createBox');
 
 /**
  * Base
@@ -167,18 +184,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Utils
  */
 const objectsToUpdate = [];
+// Sphere
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+});
+
 const createSphere = (radius, position) => {
     // Three.js mesh
-    const mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 20, 20),
-        new THREE.MeshStandardMaterial({
-            metalness: 0.3,
-            roughness: 0.4,
-            envMap: environmentMapTexture,
-            envMapIntensity: 0.5
-        })
-    )
-
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    mesh.scale.set(radius, radius, radius);
     mesh.castShadow = true;
     mesh.position.copy(position);
     scene.add(mesh);
@@ -198,7 +215,38 @@ const createSphere = (radius, position) => {
     objectsToUpdate.push({mesh, body});
 };
 
-createSphere(0.5, {x:0, y:3, z:0});
+createSphere(0.5, {x: 0, y: 3, z: 0});
+
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+});
+const createBox = (width, height, depth, position) => {
+    // Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    mesh.scale.set(width, height, depth);
+    mesh.castShadow = true;
+    mesh.position.copy(position);
+    scene.add(mesh);
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5));
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial
+    });
+    body.position.copy(position);
+    world.addBody(body);
+
+    // Save in objects to update
+    objectsToUpdate.push({mesh, body});
+}
+createBox(1, 1, 1, {x: 0, y: 3, z: 0});
 
 /**
  * Animate
@@ -215,7 +263,7 @@ const tick = () => {
     // sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
     world.step(1 / 60, deltaTime, 3);
 
-    for(const object of objectsToUpdate) {
+    for (const object of objectsToUpdate) {
         object.mesh.position.copy(object.body.position);
     }
 
